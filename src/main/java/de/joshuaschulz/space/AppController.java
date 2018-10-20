45172dcd57f72fc16e6a2a7daf0f367c98972909
Schulz <joshua.schulz@sap.com>
2018-10-20 19:50:36 +0200

    startupPage added info dialog to show extended info

--- src/main/java/de/joshuaschulz/space/AppController.java
+++ src/main/java/de/joshuaschulz/space/AppController.java
@@ -7,8 +7,10 @@ import de.joshuaschulz.connection.AsyncAPICall;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseDragEvent;
+import javafx.stage.Modality;
+import javafx.stage.StageStyle;
 
 
 import java.net.URL;
@@ -23,6 +25,7 @@ public class AppController implements Initializable {
     private Gson gson = new Gson();
 
     public Label startUpInfo;
+
     @Override
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
@@ -42,11 +45,12 @@ public class AppController implements Initializable {
         }catch (Exception e){
             e.printStackTrace();
         }
+        executor.shutdown();
     }
 
     @FXML
     protected void handleInfoButton(ActionEvent event) {
-
+        this.showInfo();
     }
     @FXML
     protected void handleEnteredInfoButton() {
@@ -56,5 +60,19 @@ public class AppController implements Initializable {
     @FXML
     protected void handleExitedInfoButton() {
         startUpInfo.setVisible(false);
+    }
+    private void showInfo(){
+        Dialog dialog = new Dialog();
+        DialogPane dialogPane = dialog.getDialogPane();
+        dialogPane.getStylesheets().add(
+                getClass().getResource("/stylesheets/Dialog.css").toExternalForm());
+        dialogPane.getStyleClass().add("myDialog");
+        dialog.setTitle(resultBackground.get("title").toString());
+        dialog.setContentText(resultBackground.get("explanation").toString());
+        ButtonType loginButtonType = new ButtonType("Close", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().add(loginButtonType);
+        dialog.initStyle(StageStyle.UTILITY);
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.showAndWait();
     }
 }
