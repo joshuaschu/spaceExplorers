20328f4fd1a773293f58df1722f013d6848e1e87
Schulz <joshua.schulz@sap.com>
2018-10-21 11:47:03 +0200

    mainpage added first panel and tableview

--- src/main/java/de/joshuaschulz/space/MainController.java
+++ src/main/java/de/joshuaschulz/space/MainController.java
@@ -1,4 +1,15 @@
 package de.joshuaschulz.space;
 
-public class MainController {
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    public JFXTreeTableView tableView;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
