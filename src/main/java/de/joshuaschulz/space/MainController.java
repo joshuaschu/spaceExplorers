4b732fd1ca367312958e1bce8428b898f93eacdb
Joshua Schulz <joshua.schulz@sap.com>
2018-10-22 09:10:05 +0200

    mainPage added getCurrentDate

--- src/main/java/de/joshuaschulz/space/MainController.java
+++ src/main/java/de/joshuaschulz/space/MainController.java
@@ -13,27 +13,24 @@ import de.joshuaschulz.connection.AsyncAPICall;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableCell;
-import javafx.scene.control.TreeTableColumn;
-import javafx.util.Callback;
 
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainController implements Initializable {
-    public JFXTreeTableView<SpaceObject> nearestObjects;
-    public JFXTreeTableColumn<SpaceObject,String> nameColumn;
-    public JFXTreeTableColumn<SpaceObject,String> closesApproach;
-    public JFXTreeTableColumn<SpaceObject,String> isHazardous;
+    public JFXTreeTableView<Asteroid> nearestObjects;
+    public JFXTreeTableColumn<Asteroid,String> nameColumn;
+    public JFXTreeTableColumn<Asteroid,String> closesApproach;
+    public JFXTreeTableColumn<Asteroid,String> isHazardous;
 
     private ExecutorService executor = Executors.newCachedThreadPool();
     private Gson gson = new Gson();
@@ -51,7 +48,7 @@ public class MainController implements Initializable {
         isHazardous.setCellValueFactory(param -> param.getValue().getValue().hazardous);
         HashMap<String,String> params= new HashMap<>();
         params.put("datailed","true");
-        ObservableList<SpaceObject> spaceObjects = FXCollections.observableArrayList();
+        ObservableList<Asteroid> spaceObjects = FXCollections.observableArrayList();
         try {
             executor.execute(new APIRequestHandler("https://api.nasa.gov/neo/rest/v1/feed/today", params, new AsyncAPICall() {
                 @Override
@@ -60,13 +57,13 @@ public class MainController implements Initializable {
                     apiResults.put("nearestObjects",jsonObject);
                     JsonElement values = jsonObject.get("near_earth_objects");
                     JsonObject objectJson = values.getAsJsonObject();
-                    JsonArray array = objectJson.getAsJsonArray("2018-10-21");
+                    JsonArray array = objectJson.getAsJsonArray(getCurrentDate());
                     for (int i = 0; i < array.size(); i++) {
-                        spaceObjects.add(new SpaceObject(array.get(i).getAsJsonObject().get("name").toString(),
+                        spaceObjects.add(new Asteroid(array.get(i).getAsJsonObject().get("name").toString(),
                                                          array.get(i).getAsJsonObject().get("close_approach_data").getAsJsonArray().get(0).getAsJsonObject().get("miss_distance").getAsJsonObject().get("kilometers").toString(),
                                                          array.get(i).getAsJsonObject().get("is_potentially_hazardous_asteroid").toString()));
                     }
-                    final TreeItem<SpaceObject> root = new RecursiveTreeItem<SpaceObject>(spaceObjects,RecursiveTreeObject::getChildren);
+                    final TreeItem<Asteroid> root = new RecursiveTreeItem<Asteroid>(spaceObjects,RecursiveTreeObject::getChildren);
                     Platform.runLater(()->{
                         nearestObjects.setRoot(root);
                         nearestObjects.setShowRoot(false);
@@ -81,53 +78,20 @@ public class MainController implements Initializable {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        executor.shutdown();
+        getCurrentDate();
     }
-
-    private boolean fetchAPI(String baseURL, Map<String,String> params){
-        if(params!=null) {
-            try {
-                executor.execute(new APIRequestHandler(baseURL, params, new AsyncAPICall() {
-                    @Override
-                    public void onSuccess(String result) {
-                    //    JsonObject jsonObject = gson.fromJson(result,JsonObject.class);
-                     //   apiResults.put(baseURL,jsonObject);
-                    }
-
-                    @Override
-                    public void onFailure(Exception exception) {
-                        apiResults.put(baseURL,null);
-                    }
-                }));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return true;
-        }else {
-            try {
-                executor.execute(new APIRequestHandler(baseURL, new AsyncAPICall() {
-                    @Override
-                    public void onSuccess(String result) {
-                        JsonObject jsonObject = gson.fromJson(result,JsonObject.class);
-                        apiResults.put(baseURL,jsonObject);
-                    }
-
-                    @Override
-                    public void onFailure(Exception exception) {
-                        apiResults.put(baseURL,null);
-                    }
-                }));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return true;
-        }
+    private String getCurrentDate(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
     }
-    class SpaceObject extends RecursiveTreeObject<SpaceObject>{
+    class Asteroid extends RecursiveTreeObject<Asteroid>{
         StringProperty name;
         StringProperty approach;
         StringProperty hazardous;
 
-        public SpaceObject(String name, String approach, String hazardous){
+        public Asteroid(String name, String approach, String hazardous){
             this.name = new SimpleStringProperty(name);
             this.approach = new SimpleStringProperty(approach);
             this.hazardous = new SimpleStringProperty(hazardous);
